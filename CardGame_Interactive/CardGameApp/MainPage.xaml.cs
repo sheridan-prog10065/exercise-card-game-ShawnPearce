d926e66ddaa9b2689aaa485860d4ec0239e5428c
ShawnPearce <pearcsha@sheridancollege.ca>
2025-07-21 09:57:51 -0400

    1.8 establish has a relationship between main page and card game

--- CardGame_Interactive/CardGameApp/MainPage.xaml.cs
+++ CardGame_Interactive/CardGameApp/MainPage.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace CardGameApp;
+﻿using CardGameApp.BusinessLogic;
+
+namespace CardGameApp;
 
 public partial class MainPage : ContentPage
+
 {
+    private CardGame _cardGame;
     public MainPage()
     {
         InitializeComponent();
+        //Created the card game
+        _cardGame = new CardGame();
     }
 }
